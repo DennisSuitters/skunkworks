47945fccf1b24dc277e45bd3e2d259f73ad6e0ec
Dennis Suitters <dennis@diemen.design>
2020-12-30 15:01:25 +1100

    Allow click on Modal Backdrop to close modal
    Removed adding the element for backdrop as I noticed it being added everytime a modal is open (every time), and not being removed. So I moved the styling to still have the darkened background under a modal, which allowed for binding an event for closing the modal when clicking the backdrop. This reduced code, and stopped spawning of unnused backdrop elements.

--- src/js/ui/ModalUI.js
+++ src/js/ui/ModalUI.js
@@ -3,11 +3,9 @@ import $ from 'jquery';
 class ModalUI {
   constructor($node /*, options */) {
     this.$modal = $node;
-    this.$backdrop = $('<div class="note-modal-backdrop"></div>');
   }
 
   show() {
-    this.$backdrop.appendTo(document.body).show();
     this.$modal.addClass('note-open').show();
     this.$modal.trigger('note.modal.show');
     this.$modal.off('click', '.note-close').on('click', '.note-close', this.hide.bind(this));
@@ -17,11 +15,15 @@ class ModalUI {
         this.hide();
       }
     });
+    this.$modal.on('click', (event) => {
+      if(event.target.classList.contains('note-open')){
+        this.hide();
+      }
+    });
   }
 
   hide() {
     this.$modal.removeClass('note-open').hide();
-    this.$backdrop.hide();
     this.$modal.trigger('note.modal.hide');
     this.$modal.off('keydown');
   }
