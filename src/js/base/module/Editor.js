75fe460e5a14c7995536d33fe04bff72901024b0
Dennis Suitters <dennis@diemen.design>
2021-11-08 01:21:09 +1100

    Fix Editor.js double Paragraph insertion
    Finally, found where the issue in Editor.js where double Paragraph's were inserted when pressing the Enter key.

--- src/js/base/module/Editor.js
+++ src/js/base/module/Editor.js
@@ -456,8 +456,10 @@ export default class Editor {
       if (this.context.invoke(eventName) !== false) {
         event.preventDefault();
         // if keyMap action was invoked
-        this.context.invoke(eventName);
-//        return true;
+        if (keyName != 'ENTER') {  // <--- Without this check, we get double Empty Paragraph insertion.
+          this.context.invoke(eventName);
+        }
+        return true;
       }
     } else if (key.isEdit(event.keyCode)) {
       if (key.isRemove(event.keyCode)) {
